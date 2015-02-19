be19a37226993cb69a1168ed8dabd27c1b1c968a
supernova2468 <supernova2468@gmail.com>
2015-02-19 22:54:30 +0000

    added install for lcd library

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+cd ~
 sudo apt-get update
 sudo apt-get install build-essential python-dev python-setuptools python-pip python-smbus ntpdate -y
 ntpdate 0.us.pool.ntp.org
@@ -6,5 +7,9 @@ chmod +x dtc.sh
 ./dtc.sh
 sudo pip install Adafruit_BBIO
 sudo pip install gspread
+git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git
+cd Adafruit_Python_CharLCD
+sudo python setup.py install
+cd ~
 git clone https://github.com/supernova2468/smartrelay.git
 cd smartrelay
