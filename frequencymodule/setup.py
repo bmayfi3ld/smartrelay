792b567d0483331d10ddb4c0aec0a1f0a4e0b7bf
Brandon <supernova2468@users.noreply.github.com>
2015-03-10 20:14:59 -0500

    using much simpler frequency calc

--- frequencymodule/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup, Extension
-
-
-setup(name='frequency_module', version='1.0',  \
-      ext_modules=[Extension('frequency_module', ['frequencymodule.c', 'event_gpio.c', 'common.c'])])
